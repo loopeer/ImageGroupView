3161b2fc4716078ea70bdd842eda81a7b93b08e6
ToDou <yytodou123@gmail.com>
2015-09-18 16:34:19 +0800

    add the screenshot

--- app/src/main/java/com/loopeer/android/apps/imagegroupview/MainActivity.java
+++ app/src/main/java/com/loopeer/android/apps/imagegroupview/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements ImageGroupView.On
 
     private ImageGroupView imageGroupAddAble;
     private ImageGroupView imageGroup;
+    private ImageGroupView imageGroup2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements ImageGroupView.On
 
     private void initView() {
         imageGroup = (ImageGroupView) findViewById(R.id.images_group);
+        imageGroup2 = (ImageGroupView) findViewById(R.id.images_group_2);
         imageGroupAddAble = (ImageGroupView) findViewById(R.id.images_group_addable);
     }
 
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements ImageGroupView.On
         imageGroupAddAble.setFragmentManager(getSupportFragmentManager());
         imageGroupAddAble.setPhotos(createTestData());
         imageGroup.setPhotos(createTestData());
+        imageGroup2.setPhotos(createTestData());
         imageGroup.setOnImageClickListener(this);
     }
 
@@ -46,7 +49,6 @@ public class MainActivity extends AppCompatActivity implements ImageGroupView.On
         result.add("http://img5.imgtn.bdimg.com/it/u=1849768669,2102852722&fm=21&gp=0.jpg");
         result.add("http://img5.imgtn.bdimg.com/it/u=138250353,211064234&fm=21&gp=0.jpg");
         result.add("http://img4.imgtn.bdimg.com/it/u=434281914,1810736344&fm=21&gp=0.jpg");
-        result.add("http://img3.imgtn.bdimg.com/it/u=4024518858,124891761&fm=21&gp=0.jpg");
         return result;
     }
 
@@ -62,6 +64,6 @@ public class MainActivity extends AppCompatActivity implements ImageGroupView.On
 
     @Override
     public void onImageClick(SquareImage clickImage, ArrayList<SquareImage> squareImages, ArrayList<String> allImageInternetUrl) {
-        Toast.makeText(this, "Inter Image size is :  " + allImageInternetUrl, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Inter Images is :  " + allImageInternetUrl, Toast.LENGTH_SHORT).show();
     }
 }
