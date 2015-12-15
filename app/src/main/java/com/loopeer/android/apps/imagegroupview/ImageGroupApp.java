34c4efc7d31d9a1edd6e8fbe50455e476d4115de
ToDou <yytodou123@gmail.com>
2015-12-15 11:29:28 +0800

    fix the screenshot load by fresco slow

--- app/src/main/java/com/loopeer/android/apps/imagegroupview/ImageGroupApp.java
+++ app/src/main/java/com/loopeer/android/apps/imagegroupview/ImageGroupApp.java
@@ -3,11 +3,17 @@ package com.loopeer.android.apps.imagegroupview;
 import android.app.Application;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
 
 public class ImageGroupApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fresco.initialize(this);
+
+        ImagePipelineConfig config = ImagePipelineConfig.newBuilder(getApplicationContext())
+                .setDownsampleEnabled(true)
+                .build();
+
+        Fresco.initialize(this, config);
     }
 }
