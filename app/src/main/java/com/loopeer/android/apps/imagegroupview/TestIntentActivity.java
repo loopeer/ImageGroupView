b615d5aae6fe99eaef6afba1a7f5b415b068b66d
ToDou <yytodou123@gmail.com>
2016-12-14 11:08:47 +0800

    add inplicit intent

---
+++ app/src/main/java/com/loopeer/android/apps/imagegroupview/TestIntentActivity.java
@@ -0,0 +1,13 @@
+package com.loopeer.android.apps.imagegroupview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_intent);
+    }
+}
--- imagegroupview/src/main/java/com/loopeer/android/librarys/imagegroupview/NavigatorImage.java
+++ imagegroupview/src/main/java/com/loopeer/android/librarys/imagegroupview/NavigatorImage.java
@@ -3,6 +3,7 @@ package com.loopeer.android.librarys.imagegroupview;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Parcelable;
 import android.provider.MediaStore;
 
@@ -14,6 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NavigatorImage {
+
+    public static final String ACTION_IMAGE_GROUP = "com.loopeer.android.librarys.imagegroupview";
+    public static final String URI_IMAGEGROUPVIEW_SWITCHER = "imagegroupview://switcher";
+    public static final String URI_IMAGEGROUPVIEW_ALBUM = "imagegroupview://album";
 
     public static final String EXTRA_PHOTO_URL = "extra_photo_url";
     public static final String EXTRA_PHOTOS_URL = "extra_photos_url";
@@ -36,31 +41,58 @@ public class NavigatorImage {
             MediaStore.Images.Media.DATA,
             MediaStore.Images.Media.DISPLAY_NAME,
             MediaStore.Images.Media.DATE_ADDED,
-            MediaStore.Images.Media._ID };
+            MediaStore.Images.Media._ID};
 
     public static void startImageSwitcherActivity(Context context, List<SquareImage> images, int position, boolean showAddButton, int placeholderDrawable, int groupId) {
-        Intent intent = new Intent(context, ImageSwitcherActivity.class);
+        Intent intent;
+        intent = new Intent(ACTION_IMAGE_GROUP);
+        intent.setData(Uri.parse(URI_IMAGEGROUPVIEW_SWITCHER));
+        if (intent == null || intent.resolveActivity(context.getPackageManager()) == null) {
+            intent = new Intent(context, ImageSwitcherActivity.class);
+        }
+        addImageSwitcherData(images, position, showAddButton, placeholderDrawable, groupId, intent);
+        ((Activity) context).startActivityForResult(intent, RESULT_IMAGE_SWITCHER);
+    }
+
+    private static void addImageSwitcherData(List<SquareImage> images, int position, boolean showAddButton, int placeholderDrawable, int groupId, Intent intent) {
         intent.putParcelableArrayListExtra(NavigatorImage.EXTRA_IMAGE_URL, new ArrayList<Parcelable>(images));
         intent.putExtra(EXTRA_IMAGE_DELETE, showAddButton);
         intent.putExtra(EXTRA_IMAGE_URL_POSITION, position);
         intent.putExtra(EXTRA_IMAGE_PLACE_DRAWABLE_ID, placeholderDrawable);
         intent.putExtra(EXTRA_IMAGE_GROUP_ID, groupId);
-        ((Activity)context).startActivityForResult(intent, RESULT_IMAGE_SWITCHER);
     }
 
     public static void startCustomAlbumActivity(Context context, int canSelectMaxNum, int groupId) {
-        Intent intent = new Intent(context, AlbumActivity.class);
-        intent.putExtra(EXTRA_IMAGE_SELECT_MAX_NUM, canSelectMaxNum);
-        intent.putExtra(EXTRA_IMAGE_GROUP_ID, groupId);
-        ((Activity)context).startActivityForResult(intent, RESULT_SELECT_PHOTOS);
+        Intent intent;
+        intent = new Intent(ACTION_IMAGE_GROUP);
+        intent.setData(Uri.parse(URI_IMAGEGROUPVIEW_ALBUM));
+        if (intent == null || intent.resolveActivity(context.getPackageManager()) == null) {
+            intent = new Intent(context, AlbumActivity.class);
+        }
+        addAlbumData(canSelectMaxNum, groupId, intent, EXTRA_IMAGE_SELECT_MAX_NUM, EXTRA_IMAGE_GROUP_ID);
+        ((Activity) context).startActivityForResult(intent, RESULT_SELECT_PHOTOS);
+    }
+
+    private static void addAlbumData(int canSelectMaxNum, int groupId, Intent intent, String extraImageSelectMaxNum, String extraImageGroupId) {
+        intent.putExtra(extraImageSelectMaxNum, canSelectMaxNum);
+        intent.putExtra(extraImageGroupId, groupId);
     }
 
     public static void startCustomAlbumActivity(Context context, int canSelectMaxNum, int groupId, int type) {
-        Intent intent = new Intent(context, AlbumActivity.class);
-        intent.putExtra(EXTRA_IMAGE_SELECT_MAX_NUM, canSelectMaxNum);
-        intent.putExtra(EXTRA_IMAGE_GROUP_ID, groupId);
-        intent.putExtra(EXTRA_ALBUM_TYPE, type);
-        ((Activity)context).startActivityForResult(intent, RESULT_SELECT_PHOTOS);
+        Intent intent;
+        intent = new Intent(ACTION_IMAGE_GROUP);
+        intent.setData(Uri.parse(URI_IMAGEGROUPVIEW_ALBUM));
+        if (intent == null || intent.resolveActivity(context.getPackageManager()) == null) {
+            intent = new Intent(context, AlbumActivity.class);
+        }
+        addAlbumDataWithType(canSelectMaxNum, groupId, type, intent, EXTRA_IMAGE_SELECT_MAX_NUM, EXTRA_IMAGE_GROUP_ID, EXTRA_ALBUM_TYPE);
+        ((Activity) context).startActivityForResult(intent, RESULT_SELECT_PHOTOS);
+    }
+
+    private static void addAlbumDataWithType(int canSelectMaxNum, int groupId, int type, Intent intent, String extraImageSelectMaxNum, String extraImageGroupId, String extraAlbumType) {
+        intent.putExtra(extraImageSelectMaxNum, canSelectMaxNum);
+        intent.putExtra(extraImageGroupId, groupId);
+        intent.putExtra(extraAlbumType, type);
     }
 
 }
