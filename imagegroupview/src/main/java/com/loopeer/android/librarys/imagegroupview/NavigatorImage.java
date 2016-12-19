d6c874800545febf14b304f886aaf89aef42813c
ToDou <yytodou123@gmail.com>
2016-12-19 17:51:18 +0800

    fix the permission

--- imagegroupview/src/main/java/com/loopeer/android/librarys/imagegroupview/NavigatorImage.java
+++ imagegroupview/src/main/java/com/loopeer/android/librarys/imagegroupview/NavigatorImage.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class NavigatorImage {
 
-    public static final String[] PERMISSION_CAMERA_STARTREQUEST = new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,"android.permission.ACCESS_COARSE_LOCATION"};
+    public static final String[] PERMISSION_CAMERA_STARTREQUEST = new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
     public static final int REQUEST_CAMERA_STARTREQUEST = 1;
 
     public static final String ACTION_IMAGE_GROUP = "com.loopeer.android.librarys.imagegroupview";
