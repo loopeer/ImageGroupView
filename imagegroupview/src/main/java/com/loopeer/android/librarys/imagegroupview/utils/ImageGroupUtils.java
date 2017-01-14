e39b8dfec25f38bed575a2e4c547e90e5d69146b
yanxin <yanxin@loopeer.com>
2017-01-14 15:34:18 +0800

    fix ImageGroupUtils

--- imagegroupview/src/main/java/com/loopeer/android/librarys/imagegroupview/utils/ImageGroupUtils.java
+++ imagegroupview/src/main/java/com/loopeer/android/librarys/imagegroupview/utils/ImageGroupUtils.java
@@ -8,15 +8,21 @@ import android.provider.MediaStore;
 public class ImageGroupUtils {
 
     public static String getPathOfPhotoByUri(Context context, Uri uri) {
-        String[] filePathColumn = { MediaStore.Images.Media.DATA };
-
-        Cursor cursor = MediaStore.Images.Media.query(context.getContentResolver(), uri, filePathColumn);
-        cursor.moveToFirst();
-
-        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-        String picturePath = cursor.getString(columnIndex);
-        cursor.close();
-        return picturePath;
+        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        Cursor cursor = null;
+        try {
+            cursor = MediaStore.Images.Media.query(context.getContentResolver(), uri, filePathColumn);
+            if (cursor != null && cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                return cursor.getString(columnIndex);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
     }
 
 }
